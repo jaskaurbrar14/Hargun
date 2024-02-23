96ede64cdd3fc287921be0940005d0b5bc43b216
jaskaurbrar14 <jaskaurbrar14@gmail.com>
2024-02-23 12:16:57 +0530

    complete the footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,51 +1,66 @@
 import "./Footer.scss";
 import { Link } from "react-router-dom";
-import hargun from "../../assets/logo/hargun.png";
-import facebook from "../../assets/icons/Icon-facebook.svg";
-import twitter from "../../assets/icons/Icon-twitter.svg";
-import instagram from "../../assets/icons/Icon-instagram.svg";
-import mail from "../../assets/icons/mail.svg";
-
 import React from "react";
 
 function Footer() {
   return (
-    <footer>
-      <article>
-        <h3>Subscribe for Offers</h3>
-        <form>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            placeholder="Your@email.com"
-            minLength="1"
-            maxLength="100"
-            required
-          ></input>
-          <button>Subscribe</button>
-        </form>
+    <footer className="footer">
+      <article className="footer__signUp">
+        <div className="footer__signUp-wrapper">
+          <h3 className="footer__signUp-wrapper-heading">
+            Subscribe for Offers
+          </h3>
+          <form className="footer__signUp-wrapper-form">
+            <input
+              className="footer__signUp-wrapper-form-input"
+              id="email"
+              type="email"
+              name="email"
+              placeholder="Your@email.com"
+              minLength="1"
+              maxLength="100"
+              required
+            ></input>
+            <button className="footer__signUp-wrapper-form-button">
+              Subscribe
+            </button>
+          </form>
+        </div>
       </article>
-      <ul>
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Store locator</a>
-        </li>
-        <li>
-          <a href="#">FAQS</a>
-        </li>
-        <li>
-          <a href="#">News</a>
-        </li>
-        <li>
-          <a href="#">Careers</a>
-        </li>
-        <li>
-          <a href="#">Contact Us</a>
-        </li>
-      </ul>
+      <section className="footer__links">
+        <ul className="footer__links-wrapper">
+          <li className="footer__links-wrapper-list">
+            <Link className="footer__links-wrapper-list-link" to="/">
+              About
+            </Link>
+          </li>
+          <li className="footer__links-wrapper-list">
+            <Link className="footer__links-wrapper-list-link" to="/">
+              Store locator
+            </Link>
+          </li>
+          <li className="footer__links-wrapper-list">
+            <Link className="footer__links-wrapper-list-link" to="/">
+              FAQS
+            </Link>
+          </li>
+          <li className="footer__links-wrapper-list">
+            <Link className="footer__links-wrapper-list-link" to="/">
+              News
+            </Link>
+          </li>
+          <li className="footer__links-wrapper-list">
+            <Link className="footer__links-wrapper-list-link" to="/">
+              Careers
+            </Link>
+          </li>
+          <li className="footer__links-wrapper-list">
+            <Link className="footer__links-wrapper-list-link" to="/">
+              Contact Us
+            </Link>
+          </li>
+        </ul>{" "}
+      </section>
     </footer>
   );
 }
