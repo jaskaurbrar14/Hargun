41ca8e82ecb8b736b7a8b5d2528cdbcb92f06aa9
jaskaurbrar14 <jaskaurbrar14@gmail.com>
2024-02-24 20:35:59 +0530

    made crousel responsive

--- src/components/trendingCarousel/TrendingCarousel.js
+++ src/components/trendingCarousel/TrendingCarousel.js
@@ -4,14 +4,52 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import { useProductsContext } from "../../utils/hooks.js";
+
+function Arrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "#5e5d5d" }}
+      onClick={onClick}
+    />
+  );
+}
+
 function TrendingCarousel() {
   const { trendingProducts } = useProductsContext();
   var settings = {
     dots: true,
-    infinite: false,
-    speed: 700,
-    slidesToShow: 5,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
     slidesToScroll: 3,
+    initialSlide: 0,
+    nextArrow: <Arrow />,
+    prevArrow: <Arrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
   return (
     <aside className="aside">
@@ -31,10 +69,10 @@ function TrendingCarousel() {
                   alt={product.title}
                 ></img>
                 <section className="aside__carousel-card-link-info">
-                  <p className="aside__carousel-card-link-image-info-title">
+                  <p className="aside__carousel-card-link-info-title">
                     {product.title}
                   </p>
-                  <p className="aside__carousel-card-link-image-info-price">
+                  <p className="aside__carousel-card-link-info-price">
                     {product.price}
                   </p>
                 </section>
