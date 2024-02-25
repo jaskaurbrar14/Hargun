1605cc25873d6f9f9ea56ecddbbeb3268a5a4af4
jaskaurbrar14 <jaskaurbrar14@gmail.com>
2024-02-25 18:09:03 +0530

    update product detail hooks and JSX structure

--- src/components/productDetails/ProductDetails.js
+++ src/components/productDetails/ProductDetails.js
@@ -14,41 +14,73 @@ function ProductDetails() {
     }
   }, [productId]);
   return (
-    <main className="">
-      <h1 className="">{selectedProduct.title}</h1>
-      <article key={selectedProduct.id} className="">
-        {/* <div>
+    <main className="productDetail">
+      <h1 className="productDetail__heading">{selectedProduct.title}</h1>
+      <article key={selectedProduct.id} className="productDetail__article">
+        <div className="productDetail__article-images">
           <img
-            className=""
+            className="productDetail__article-images-main"
             src={selectedProduct.photo[0]}
             alt={selectedProduct.title}
           ></img>{" "}
-          <div>
+          <div className="productDetail__article-images-sec">
             <img
-              className=""
+              className="productDetail__article-images-sec-1"
               src={selectedProduct.photo[1]}
               alt={selectedProduct.title}
             ></img>{" "}
             <img
-              className=""
+              className="productDetail__article-images-sec-2"
               src={selectedProduct.photo[2]}
               alt={selectedProduct.title}
             ></img>{" "}
             <img
-              className=""
+              className="productDetail__article-images-sec-3"
               src={selectedProduct.photo[3]}
               alt={selectedProduct.title}
             ></img>{" "}
           </div>
-        </div> */}
-        <section className="">
-          <p className="">{selectedProduct.description}</p>
-          <p className="">${selectedProduct.price}</p>
-          <div>
-            <button type="submit" className="">
+        </div>
+        <section className="productDetail__article-details">
+          <p className="productDetail__article-details-description">
+            {selectedProduct.description}
+          </p>
+          <div className="productDetail__article-details-pricing">
+            <p className="productDetail__article-details-pricing-quantity">
+              Quantity
+            </p>
+            <div className="productDetail__article-details-pricing-numbers">
+              <button
+                className="productDetail__article-details-pricing-numbers-minus"
+                type="submit"
+              >
+                {" "}
+                -{" "}
+              </button>
+              <p className="productDetail__article-details-pricing-numbers-value"></p>
+              <button
+                type="submit"
+                className="productDetail__article-details-pricing-numbers-plus"
+              >
+                {" "}
+                +{" "}
+              </button>
+            </div>
+            <p className="productDetail__article-details-pricing-price">
+              ${selectedProduct.price}
+            </p>
+          </div>
+          <div className="productDetail__article-details-cta">
+            <button
+              type="submit"
+              className="productDetail__article-details-cta-cart"
+            >
               Add to Cart
             </button>
-            <button type="submit" className="">
+            <button
+              type="submit"
+              className="productDetail__articlep-details-cta-buy"
+            >
               Buy Now
             </button>
           </div>
