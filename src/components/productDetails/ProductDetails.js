d6a089c40dc2c21386a243a6c7e3d83dee4dce2e
jaskaurbrar14 <jaskaurbrar14@gmail.com>
2024-02-28 04:43:04 +0530

    fix bugs for the photos is product detail page

--- src/components/productDetails/ProductDetails.js
+++ src/components/productDetails/ProductDetails.js
@@ -2,15 +2,9 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import "./ProductDetails.scss";
 import { useParams } from "react-router-dom";
-// import bangles0 from "../../assets/images/ChainedChoker/1.jpg";
-// import bangles1 from "../../assets/images/ChainedChoker/2.jpg";
-// import bangles2 from "../../assets/images/ChainedChoker/3.jpg";
-// import bangles3 from "../../assets/images/ChainedChoker/4.jpg";
 
-// imported the pictures for products seperately as I was having internet issues and thr pictures were'nt loading.
-// leaving the commented code to test with pictures from frontend incase the picturess take long or load from the backend.
 function ProductDetails() {
-  const [selectedProduct, setSelectedProduct] = useState({});
+  const [selectedProduct, setSelectedProduct] = useState(null);
   const [quantity, setQuantity] = useState(1);
 
   const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
@@ -43,111 +37,114 @@ function ProductDetails() {
     }
   };
 
+  // function to add products into cart
+  // const submitAddToCart = async (res, event) => {
+  //   event.preventDefault();
+  //   const data = {
+  //     product_id: selectedProduct.id,
+  //     product_quantity: quantity,
+  //     product_quantity: selectedProduct.id,
+  //   };
+  //   try {
+  //     await axios.post(`${REACT_APP_SERVER_URL}/cart`, data);
+  //     if (res.data.status === 201) {
+  //       swal({
+  //         icon: "success",
+  //         text: `"success" ${res.data.message} "success"`,
+  //       });
+  //     } else if (res.data.status === 409) {
+  //       swal(`"warning") ${res.data.message} "warning"`);
+  //     } else if (res.data.status === 401) {
+  //       swal(`"error") ${res.data.message} "error"`);
+  //     }
+  //   } catch (err) {
+  //     console.error({ err });
+  //   }
+  // };
   return (
-    <main className="productDetail">
-      <h1 className="productDetail__heading">{selectedProduct.title}</h1>
-      <article key={selectedProduct.id} className="productDetail__article">
-        <div className="productDetail__article-images">
-          <img
-            className="productDetail__article-images-main"
-            src={selectedProduct.photo[0]}
-            alt={selectedProduct.title}
-          ></img>{" "}
-          <div className="productDetail__article-images-sec">
-            <img
-              className="productDetail__article-images-sec-1"
-              src={selectedProduct.photo[1]}
-              alt={selectedProduct.title}
-            ></img>{" "}
-            <img
-              className="productDetail__article-images-sec-2"
-              src={selectedProduct.photo[2]}
-              alt={selectedProduct.title}
-            ></img>{" "}
-            <img
-              className="productDetail__article-images-sec-3"
-              src={selectedProduct.photo[3]}
-              alt={selectedProduct.title}
-            ></img>{" "}
-          </div>
-        </div>
-        {/* <div className="productDetail__article-images">
-          <img
-            className="productDetail__article-images-main"
-            src={bangles0}
-            alt={selectedProduct.title}
-          ></img>{" "}
-          <div className="productDetail__article-images-sec">
-            <img
-              className="productDetail__article-images-sec-1"
-              src={bangles1}
-              alt={selectedProduct.title}
-            ></img>{" "}
-            <img
-              className="productDetail__article-images-sec-2"
-              src={bangles2}
-              alt={selectedProduct.title}
-            ></img>{" "}
-            <img
-              className="productDetail__article-images-sec-3"
-              src={bangles3}
-              alt={selectedProduct.title}
-            ></img>{" "}
-          </div>
-        </div> */}
-        <section className="productDetail__article-details">
-          <p className="productDetail__article-details-description">
-            {selectedProduct.description}
-          </p>
-          <div className="productDetail__article-details-pricing">
-            <div className="productDetail__article-details-pricing-container">
-              <p className="productDetail__article-details-pricing-container-quantity">
-                Quantity
-              </p>
-              <div className="productDetail__article-details-pricing-container-numbers">
-                <button
-                  className="productDetail__article-details-pricing-container-numbers-minus"
-                  type="button"
-                  onClick={handleDecrement}
-                >
-                  {" "}
-                  -{" "}
-                </button>
-                <div className="productDetail__article-details-pricing-container-numbers-value">
-                  {" "}
-                  {quantity}
-                </div>
-                <button
-                  type="button"
-                  onClick={handleIncrement}
-                  className="productDetail__article-details-pricing-container-numbers-plus"
-                >
-                  {" "}
-                  +{" "}
-                </button>
+    <>
+      {selectedProduct && (
+        <main className="productDetail">
+          <h1 className="productDetail__heading">{selectedProduct.title}</h1>
+          <article key={selectedProduct.id} className="productDetail__article">
+            <div className="productDetail__article-images">
+              <img
+                className="productDetail__article-images-main"
+                src={selectedProduct.photo[0]}
+                alt={selectedProduct.title}
+              ></img>{" "}
+              <div className="productDetail__article-images-sec">
+                {selectedProduct.photo.map((photo, index) => {
+                  if (index >= 1) {
+                    return (
+                      <img
+                        key={index}
+                        className={`productDetail__article-images-sec-${index}`}
+                        src={photo}
+                        alt="product photo"
+                      />
+                    );
+                  }
+                  return null;
+                })}
               </div>
             </div>
-            <p className="productDetail__article-details-pricing-price">
-              ${selectedProduct.price}
-            </p>
-          </div>
-          <div className="productDetail__article-details-cta">
-            <button
-              type="submit"
-              className="productDetail__article-details-cta-cart"
-            >
-              Add to Cart
-            </button>
-            <button
-              type="submit"
-              className="productDetail__article-details-cta-buy"
-            >
-              Buy Now
-            </button>
-          </div>
-        </section>
-      </article>
-    </main>
+            <section className="productDetail__article-details">
+              <p className="productDetail__article-details-description">
+                {selectedProduct.description}
+              </p>
+              <div className="productDetail__article-details-pricing">
+                <div className="productDetail__article-details-pricing-container">
+                  <p className="productDetail__article-details-pricing-container-quantity">
+                    Quantity
+                  </p>
+                  <div className="productDetail__article-details-pricing-container-numbers">
+                    <button
+                      className="productDetail__article-details-pricing-container-numbers-minus"
+                      type="button"
+                      onClick={handleDecrement}
+                    >
+                      {" "}
+                      -
+                    </button>
+                    <div className="productDetail__article-details-pricing-container-numbers-value">
+                      {" "}
+                      {quantity}
+                    </div>
+                    <button
+                      type="button"
+                      onClick={handleIncrement}
+                      className="productDetail__article-details-pricing-container-numbers-plus"
+                    >
+                      {" "}
+                      +{" "}
+                    </button>
+                  </div>
+                </div>
+                <p className="productDetail__article-details-pricing-price">
+                  ${selectedProduct.price}
+                </p>
+              </div>
+              <div className="productDetail__article-details-cta">
+                <button
+                  type="submit"
+                  // onClick={submitAddToCart}
+                  className="productDetail__article-details-cta-cart"
+                >
+                  Add to Cart
+                </button>
+                <button
+                  type="submit"
+                  className="productDetail__article-details-cta-buy"
+                >
+                  Buy Now
+                </button>
+              </div>
+            </section>
+          </article>
+        </main>
+      )}
+    </>
   );
 }
 
